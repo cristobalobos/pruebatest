7b318b9608a4d3578a6a32d3a21598d5a6a4d455
cristobalobos <cristobal.lobost@gmail.com>
2020-03-16 20:16:01 -0300

    controlador

--- IConstruye.Demo.Hotel.Api/Controllers/HotelsController.cs
+++ IConstruye.Demo.Hotel.Api/Controllers/HotelsController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Search.Models;
-
+using Microsoft.Azure.Search;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IConstruye.Demo.Hotel.Api.Controllers
 {
@@ -10,17 +14,43 @@ namespace IConstruye.Demo.Hotel.Api.Controllers
     [Route("[controller]")]
     public class HotelsController : ControllerBase
     {
-       
+     
+        private readonly IConfiguration config;
+        private static SearchServiceClient _serviceClient;
+        private static ISearchIndexClient _indexClient;
+
+        [BindProperty]
+        public string SearchText { get; set; }
         public HotelsController()
         {
-           
+   
+            string searchServiceName = config["SearchServiceName"];
+            string queryApiKey = config["SearchServiceQueryApiKey"];
+
+            // Create a service and index client.
+            _serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(queryApiKey));
+            _indexClient = _serviceClient.Indexes.GetClient("hotels");
         }
 
+        public IList<SearchResult<Models.Hotel>> ResultList { get; set; }
+
+
         [HttpGet]
-        public  IList<SearchResult<Models.Hotel>> Get(string searchText)
+        public IList<SearchResult<Models.Hotel>> Get(string searchText)
         {
-            throw new NotImplementedException();
+            var parameters = new SearchParameters
+            {
+                // Enter Hotel property names into this list so only these values will be returned.
+                // If Select is empty, all values will be returned, which can be inefficient.
+                Select = new[] { "HotelName", "Description" }
+            };
+
+            var results = _indexClient.Documents.SearchAsync<Models.Hotel>(SearchText, parameters);
+            ResultList = results.Result.Results;
+            return ResultList;
         }
+
+
 
     }
 }
